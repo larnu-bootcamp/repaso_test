03963aca87e1c7ee3424dd611569cc0ede2a2963
Emiliano Rotta <emilianorottaf@gmail.com>
2022-10-26 11:47:06 -0300

    prueba de CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import React from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
-function App   () {
-  const [    count, setCount   ] = useState    (0);
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div> 
-                 <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
