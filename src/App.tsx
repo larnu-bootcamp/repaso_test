23ac19c4aae47271336e556a1c830021152bab78
Emiliano Rotta <emilianorottaf@gmail.com>
2022-10-26 11:43:30 -0300

    prueba de CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import React from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+function App   () {
+  const [    count, setCount   ] = useState    (0);
 
   return (
     <div className="App">
       <div> 
-      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-      <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
+                 <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-      <button data-testid="button" onClick={() => setCount((count) => count + 2)}>
+        <button data-testid="button" onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
         <p>
