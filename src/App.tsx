df091f4365ba384df6ab45c33df44e214a0a949b
Emiliano Rotta <emilianorottaf@gmail.com>
2022-10-27 12:00:33 -0300

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
+  return ( 
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
